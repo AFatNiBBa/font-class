8a443c0e37d5b689d00171ca442c1c362421f5ad
Sean Alunni <sean.alunni@gbsoftware.it>
2024-10-23 14:53:26 +0200

    Aggiunto `Text`

--- package/component/src/index.tsx
+++ package/component/src/index.tsx
@@ -1,8 +1,9 @@
 
 import anim from "./style/anim.module.scss";
 import generic from "./style/generic.module.scss";
+import internal from "./style/internal.module.scss";
 
-import { ComponentProps, splitProps } from "solid-js";
+import { ComponentProps, createSignal, onCleanup, ParentProps, splitProps } from "solid-js";
 
 export * from "./custom";
 export { anim, generic };
@@ -23,4 +24,22 @@ export function Icon(props: { size?: string | number } & ComponentProps<"svg">) 
             {...other}
         />
     </>
+}
+
+/**
+ * Component that allows you to align an {@link Icon} with the surrounding text.
+ * It displays 2 spans:
+ * - The first one is used to measure the height of the text
+ * - The second one is used to align the content with the text (Which would change its height, so it's forced to be the same height as the first one)
+ */
+export function Text(props: ParentProps) {
+	const [ height, setHeight ] = createSignal(0);
+	const obs = new ResizeObserver(([ x ]) => setHeight(x.target.getBoundingClientRect().height)); // I can't use the sizes inside of `x` because they are unset for inline elements
+	onCleanup(() => obs.disconnect());
+	return <>
+		<span ref={x => obs.observe(x)} />
+		<span class={internal.text} style={{ height: `${height()}px` }}>
+			{props.children}
+		</span>
+	</>
 }
